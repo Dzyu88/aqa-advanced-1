533e36893ca6dd27de7da7613e0db7ea81747a03
Dzyu88 <you@example.comdzyu88@gmail.com>
2023-09-16 17:01:30 +0300

    added task4 with fixed errors (added console.log). HW #4

--- js_functions/task4.js
+++ js_functions/task4.js
@@ -4,24 +4,22 @@ function divide (numerator, denominator){
             throw new Error("both arguments must be a number")
         if (denominator === 0)
             throw new Error("Denominator can't be 0")
-
         return numerator/denominator;
-
     } catch (err) {
         console.log("Error has been happened:", err.message)
     } finally {
         console.log("Work has been done successfully!")
     }
 }
-divide(10,2);
-
-divide(10,0);
-
-divide(0, 5);
-
-divide(10, "Hello");
-
-divide(10, true);
-
-divide(false, 4);
-
+const res1 = divide(10,2);
+console.log(res1)
+const res2 = divide(10,0);
+console.log(res2)
+const res3 = divide(0, 5);
+console.log(res3)
+const res4 = divide(10, "Hello");
+console.log(res4)
+const res5 = divide(10, true);
+console.log(res5)
+const res6 = divide(false, 4);
+console.log(res6)
